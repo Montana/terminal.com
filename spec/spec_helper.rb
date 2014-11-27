731eb3dbeb23d7e3e4d53572e912ac31cca6088d
James C Russell <james@101ideas.cz>
2014-11-27 11:04:34 +0100

    Global helpers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,31 @@
 require 'vcr'
+require 'yaml'
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures'
+  config.cassette_library_dir = 'spec/fixtures'
   config.hook_into :webmock # or :fakeweb
+end
+
+RSpec.configure do |config|
+  config.include Module.new {
+    def credentials
+      @credentials ||= YAML.load_file('spec/fixtures/credentials.yml')
+    end
+
+    def user_token
+      credentials[:user_token]
+    end
+
+    def access_token
+      credentials[:access_token]
+    end
+
+    def vcr(&block)
+      VCR.use_cassette('terminal.com', &block)
+    end
+
+    def ubuntu_snap_id
+      '987f8d702dc0a6e8158b48ccd3dec24f819a7ccb2756c396ef1fd7f5b34b7980'
+    end
+  }
 end
--- spec/terminal.com_spec.rb
+++ spec/terminal.com_spec.rb
@@ -1,12 +1,7 @@
 require 'spec_helper'
+require 'terminal.com'
 
 describe Terminal::API do
-  let(:ubuntu_snap_id) { '987f8d702dc0a6e8158b48ccd3dec24f819a7ccb2756c396ef1fd7f5b34b7980' }
-
-  def vcr(&block)
-    VCR.use_cassette('terminal.com', &block)
-  end
-
   describe 'BROWSE SNAPSHOTS & USERS' do
     describe '.get_snapshot(snapshot_id)' do
       it 'fetches info about the snapshot' do
