33f90f7689a118757b0a4a798680fa732086dab4
James C Russell <james@101ideas.cz>
2014-12-01 19:04:35 +0100

    Many more specs.

--- spec/terminal.com_spec.rb
+++ spec/terminal.com_spec.rb
@@ -231,53 +231,137 @@ describe Terminal::API do
       end
 
       context 'with given cpu and ram' do
-        it 'should start given snapshot' do
+        it 'should start given snapshot with cpu 2 (max) and ram 256' do
           response = VCR.use_cassette('start_snapshot_valid_cpu_ram') do
             snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
             described_class.start_snapshot(user_token, access_token, snapshot_id, cpu: '2 (max)', ram: 256)
           end
 
-          sleep 15.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_valid_cpu_ram.yml')
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_valid_cpu_ram.yml')
 
           response = VCR.use_cassette('start_snapshot_request_progress_valid_cpu_ram') do
             described_class.request_progress(response['request_id'])
           end
 
-          expect(response['status']).to eql('success')
           expect(response['result']['cpu']).to eql('2 (max)')
           expect(response['result']['ram']).to eql(256)
         end
 
-        it 'should start given snapshot' do
+        it 'should start given snapshot with cpu 200 and ram 3200' do
           response = VCR.use_cassette('start_snapshot_valid_cpu_ram_2') do
             snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
-            described_class.start_snapshot(user_token, access_token, snapshot_id, cpu: 200, ram: 800)
+            described_class.start_snapshot(user_token, access_token, snapshot_id, cpu: 200, ram: 3200)
           end
 
-          request_id = response['request_id']
-
-          sleep 16 unless File.exist?('spec/fixtures/start_snapshot_request_progress_valid_cpu_ram_2.yml')
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_valid_cpu_ram_2.yml')
 
           response = VCR.use_cassette('start_snapshot_request_progress_valid_cpu_ram_2') do
-            described_class.request_progress(request_id)
+            described_class.request_progress(response['request_id'])
           end
 
-          p response
-
-          # :cpu, :ram, :temporary, :name, :autopause, :startup_script, :custom_data)
-          #   "result"=>{"cpu"=>"2 (max)", "ram"=>256, "diskspace"=>10,
-          #     "name"=>"Ubuntu 14.04 Base Dev Snapshot",
-          #     "snapshot_id"=>"57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234",
-          #     "status"=>"running", "allow_spot"=>false,
-          #     "container_key"=>"d6e9e1ee-d334-4027-b365-5d7eebe5a1d7",
-          #     "subdomain"=>"botanicus221", "container_ip"=>"240.3.42.64",
-          #     "creation_time"=>1417452412482}}
-          # expect(response['result']).to
-          expect(response['status']).to eql('created')
           expect(response['result']['cpu']).to eql(200)
-          expect(response['result']['ram']).to eql(800)
+          expect(response['result']['ram']).to eql(3200)
         end
       end
+
+      context 'with given name' do
+        it 'should start given snapshot' do
+          response = VCR.use_cassette('start_snapshot_with_name') do
+            snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
+            described_class.start_snapshot(user_token, access_token, snapshot_id, name: 'QA: User Listing #356')
+          end
+
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_with_name.yml')
+
+          response = VCR.use_cassette('start_snapshot_request_progress_with_name') do
+            described_class.request_progress(response['request_id'])
+          end
+
+          expect(response['result']['name']).to eql('QA: User Listing #356')
+        end
+      end
+
+      context 'with autopause on' do
+        it 'should start given snapshot' do
+          response = VCR.use_cassette('start_snapshot_with_autopause_on') do
+            snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
+            described_class.start_snapshot(user_token, access_token, snapshot_id, autopause: true)
+          end
+
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_with_autopause_on.yml')
+
+          response = VCR.use_cassette('start_snapshot_request_progress_with_autopause_on') do
+            described_class.request_progress(response['request_id'])
+          end
+
+          # Autopause isn't propagated to the result object.
+          # If this succeeded, we expect everything to be OK.
+        end
+      end
+
+      context 'with autopause off' do
+        it 'should start given snapshot' do
+          response = VCR.use_cassette('start_snapshot_with_autopause_off') do
+            snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
+            described_class.start_snapshot(user_token, access_token, snapshot_id, autopause: false)
+          end
+
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_with_autopause_off.yml')
+
+          response = VCR.use_cassette('start_snapshot_request_progress_with_autopause_off') do
+            described_class.request_progress(response['request_id'])
+          end
+
+          # Autopause isn't propagated to the result object.
+          # If this succeeded, we expect everything to be OK.
+        end
+      end
+
+      context 'with temporary on' do
+        it 'should start given snapshot' do
+          response = VCR.use_cassette('start_snapshot_with_temporary_on') do
+            snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
+            described_class.start_snapshot(user_token, access_token, snapshot_id, temporary: true)
+          end
+
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_with_temporary_on.yml')
+
+          response = VCR.use_cassette('start_snapshot_request_progress_with_temporary_on') do
+            described_class.request_progress(response['request_id'])
+          end
+
+          # Temporary isn't propagated to the result object.
+          # If this succeeded, we expect everything to be OK.
+        end
+      end
+
+      context 'with temporary off' do
+        it 'should start given snapshot' do
+          response = VCR.use_cassette('start_snapshot_with_temporary_off') do
+            snapshot_id = '57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234'
+            described_class.start_snapshot(user_token, access_token, snapshot_id, temporary: false)
+          end
+
+          sleep 16.5 unless File.exist?('spec/fixtures/start_snapshot_request_progress_with_temporary_off.yml')
+
+          response = VCR.use_cassette('start_snapshot_request_progress_with_temporary_off') do
+            described_class.request_progress(response['request_id'])
+          end
+
+          # Temporary isn't propagated to the result object.
+          # If this succeeded, we expect everything to be OK.
+        end
+      end
+
+      # :temporary, :startup_script, :custom_data)
+      #
+      #   "result"=>{"cpu"=>"2 (max)", "ram"=>256, "diskspace"=>10,
+      #     "name"=>"Ubuntu 14.04 Base Dev Snapshot",
+      #     "snapshot_id"=>"57eff3574ac8d438224dc3aa1c6431a0dbac849a0c254e89be2e758d8113c234",
+      #     "status"=>"running", "allow_spot"=>false,
+      #     "container_key"=>"d6e9e1ee-d334-4027-b365-5d7eebe5a1d7",
+      #     "subdomain"=>"botanicus221", "container_ip"=>"240.3.42.64",
+      #     "creation_time"=>1417452412482}}
     end
   end
 end
