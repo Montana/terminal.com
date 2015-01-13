dd21bf3cbf98f632812b7a9c568079f5577b68e5
James C Russell <james@101ideas.cz>
2015-01-13 19:40:22 +0100

    This has been changed.

--- lib/terminal.com/api.rb
+++ lib/terminal.com/api.rb
@@ -23,11 +23,11 @@ module Terminal
     ###############################
 
     def list_terminals
-      Terminal::API.list_terminals(@user_token, @access_token)
+      Terminal.list_terminals(@user_token, @access_token)
     end
 
     def get_terminal(**options)
-      Terminal::API.get_terminal(@user_token, @access_token, **options)
+      Terminal.get_terminal(@user_token, @access_token, **options)
     end
 
     def start_snapshot(snapshot_id, **options)
@@ -40,27 +40,27 @@ module Terminal
         options.merge!(instance_opts)
       end
 
-      Terminal::API.start_snapshot(@user_token, @access_token, snapshot_id, **options)
+      Terminal.start_snapshot(@user_token, @access_token, snapshot_id, **options)
     end
 
     def delete_terminal(container_key)
-      Terminal::API.delete_terminal(@user_token, @access_token, container_key)
+      Terminal.delete_terminal(@user_token, @access_token, container_key)
     end
 
     def restart_terminal(container_key)
-      Terminal::API.restart_terminal(@user_token, @access_token, container_key)
+      Terminal.restart_terminal(@user_token, @access_token, container_key)
     end
 
     def pause_terminal(container_key)
-      Terminal::API.pause_terminal(@user_token, @access_token, container_key)
+      Terminal.pause_terminal(@user_token, @access_token, container_key)
     end
 
     def resume_terminal(container_key)
-      Terminal::API.resume_terminal(@user_token, @access_token, container_key)
+      Terminal.resume_terminal(@user_token, @access_token, container_key)
     end
 
     def edit_terminal(container_key, **options)
-      Terminal::API.edit_terminal(@user_token, @access_token, container_key, **options)
+      Terminal.edit_terminal(@user_token, @access_token, container_key, **options)
     end
 
     ###############################
@@ -68,23 +68,23 @@ module Terminal
     ###############################
 
     def list_snapshots(**options)
-      Terminal::API.list_snapshots(@user_token, @access_token, **options)
+      Terminal.list_snapshots(@user_token, @access_token, **options)
     end
 
     def count_snapshots(**options)
-      Terminal::API.count_snapshots(@user_token, @access_token, **options)
+      Terminal.count_snapshots(@user_token, @access_token, **options)
     end
 
     def delete_snapshot(snapshot_id)
-      Terminal::API.delete_snapshot(@user_token, @access_token, snapshot_id)
+      Terminal.delete_snapshot(@user_token, @access_token, snapshot_id)
     end
 
     def edit_snapshot(snapshot_id, **options)
-      Terminal::API.edit_snapshot(@user_token, @access_token, snapshot_id, **options)
+      Terminal.edit_snapshot(@user_token, @access_token, snapshot_id, **options)
     end
 
     def snapshot_terminal(container_key, **options)
-      Terminal::API.snapshot_terminal(@user_token, @access_token, container_key, **options)
+      Terminal.snapshot_terminal(@user_token, @access_token, container_key, **options)
     end
 
     ##########################
@@ -92,19 +92,19 @@ module Terminal
     ##########################
 
     def add_terminal_links(container_key, *links)
-      Terminal::API.add_terminal_links(@user_token, @access_token, container_key, *links)
+      Terminal.add_terminal_links(@user_token, @access_token, container_key, *links)
     end
 
     def remove_terminal_links(container_key, *links)
-      Terminal::API.remove_terminal_links(@user_token, @access_token, container_key, *links)
+      Terminal.remove_terminal_links(@user_token, @access_token, container_key, *links)
     end
 
     def list_terminal_access(container_key)
-      Terminal::API.list_terminal_access(@user_token, @access_token, container_key)
+      Terminal.list_terminal_access(@user_token, @access_token, container_key)
     end
 
     def edit_terminal_access(container_key, **options)
-      Terminal::API.edit_terminal_access(@user_token, @access_token, container_key, **options)
+      Terminal.edit_terminal_access(@user_token, @access_token, container_key, **options)
     end
 
     #################################
@@ -112,23 +112,23 @@ module Terminal
     #################################
 
     def get_cname_records
-      Terminal::API.get_cname_records(@user_token, @access_token)
+      Terminal.get_cname_records(@user_token, @access_token)
     end
 
     def add_domain_to_pool
-      Terminal::API.add_domain_to_pool(@user_token, @access_token, domain)
+      Terminal.add_domain_to_pool(@user_token, @access_token, domain)
     end
 
     def remove_domain_from_pool
-      Terminal::API.remove_domain_from_pool(@user_token, @access_token, domain)
+      Terminal.remove_domain_from_pool(@user_token, @access_token, domain)
     end
 
     def add_cname_record(domain, subdomain, port)
-      Terminal::API.add_cname_record(@user_token, @access_token, domain, subdomain, port)
+      Terminal.add_cname_record(@user_token, @access_token, domain, subdomain, port)
     end
 
     def remove_cname_record(domain)
-      Terminal::API.remove_cname_record(@user_token, @access_token, domain)
+      Terminal.remove_cname_record(@user_token, @access_token, domain)
     end
 
     #################################
@@ -136,39 +136,39 @@ module Terminal
     #################################
 
     def set_terminal_idle_settings(container_key, action, *triggers)
-      Terminal::API.set_terminal_idle_settings(@user_token, @access_token, action, *triggers)
+      Terminal.set_terminal_idle_settings(@user_token, @access_token, action, *triggers)
     end
 
     def get_terminal_idle_settings(container_key)
-      Terminal::API.get_terminal_idle_settings(@user_token, @access_token, container_key)
+      Terminal.get_terminal_idle_settings(@user_token, @access_token, container_key)
     end
 
     def balance
-      Terminal::API.balance(@user_token, @access_token)
+      Terminal.balance(@user_token, @access_token)
     end
 
     def balance_added
-      Terminal::API.balance_added(@user_token, @access_token)
+      Terminal.balance_added(@user_token, @access_token)
     end
 
     def gift(email, cents)
-      Terminal::API.gift(@user_token, @access_token, email, cents)
+      Terminal.gift(@user_token, @access_token, email, cents)
     end
 
     def burn_history
-      Terminal::API.burn_history(@user_token, @access_token)
+      Terminal.burn_history(@user_token, @access_token)
     end
 
     def terminal_usage_history
-      Terminal::API.terminal_usage_history(@user_token, @access_token)
+      Terminal.terminal_usage_history(@user_token, @access_token)
     end
 
     def burn_state
-      Terminal::API.burn_state(@user_token, @access_token)
+      Terminal.burn_state(@user_token, @access_token)
     end
 
     def burn_estimates
-      Terminal::API.burn_estimates(@user_token, @access_token)
+      Terminal.burn_estimates(@user_token, @access_token)
     end
 
     ##########################
@@ -176,19 +176,19 @@ module Terminal
     ##########################
 
     def add_authorized_key_to_terminal(container_key, publicKey)
-      Terminal::API.add_authorized_key_to_terminal(@user_token, @access_token, container_key, publicKey)
+      Terminal.add_authorized_key_to_terminal(@user_token, @access_token, container_key, publicKey)
     end
 
     def add_authorized_key_to_ssh_proxy(name, publicKey)
-      Terminal::API.add_authorized_key_to_ssh_proxy(@user_token, @access_token, name, publicKey)
+      Terminal.add_authorized_key_to_ssh_proxy(@user_token, @access_token, name, publicKey)
     end
 
     def del_authorized_key_from_ssh_proxy(name, fingerprint)
-      Terminal::API.del_authorized_key_from_ssh_proxy(@user_token, @access_token, name, fingerprint)
+      Terminal.del_authorized_key_from_ssh_proxy(@user_token, @access_token, name, fingerprint)
     end
 
     def get_authorized_key_from_ssh_proxy
-      Terminal::API.get_authorized_key_from_ssh_proxy(@user_token, @access_token)
+      Terminal.get_authorized_key_from_ssh_proxy(@user_token, @access_token)
     end
 
     #########
@@ -196,7 +196,7 @@ module Terminal
     #########
 
     def who_am_i
-      Terminal::API.who_am_i(@user_token, @access_token)
+      Terminal.who_am_i(@user_token, @access_token)
     end
   end
 end
